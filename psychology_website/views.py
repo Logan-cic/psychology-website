a01d9a9442350cbe624b2262dddb0d8480844ada
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-01-10 21:49:12 -0300

    register in the app in settings and using the render function in the view

--- psychology_website/views.py
+++ psychology_website/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'home.html')
